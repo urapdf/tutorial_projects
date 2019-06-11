db76b93b5a10c477b492b0e999bc1fa14974e64d
urapdf <urapdf@yahoo.com>
2019-06-11 06:31:22 -0700

    add xpath code

--- scrape_youtube/initial_scrape.py
+++ scrape_youtube/initial_scrape.py
@@ -9,12 +9,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-#variable
-my_site = "https://www.youtube.com/results?search_query=moloko&sp=EgIQAQ%253D%253D"
+#variables
+youtube_test_site = "https://www.youtube.com/results?search_query=moloko&sp=EgIQAQ%253D%253D"
+travel_search_string = "https://www.youtube.com/results?search_query=Travel&sp=EgIQAQ%253D%253D"
 
 #Test code
 driver = webdriver.Chrome()
-driver.get(my_site)
+driver.get(travel_search_string)
+
+
+user_data = driver.find_elements_by_xpath('//*[@id="video-title"]')
+links = []
+for i in user_data:
+    links.append(i.get_attribute('href'))
+
+print(len(links))
+
 
 
 
